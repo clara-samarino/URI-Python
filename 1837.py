424b29f18995afa35d2918280fbe865dad0fb63c
ANA CLARA SAMARINO <72000899@aluno.faculdadecotemig.br>
2021-05-19 11:20:34 -0300

    add 1837

--- 1837.py
+++ 1837.py
@@ -1,11 +1,14 @@
+import sys
 numeros = input().split()
 a,b = list(map(lambda x: int(x),numeros))
 
-if a > b:
-    if b < 0
-    r = a%b
-    q = int((a - r)/b)
-    print(q, r)
-else:
-
-# a = b*q + r
+maiorValor = max(abs(a),abs(b))
+qs = 0
+rs = sys.maxsize
+r = 0
+for q in range(-maiorValor,maiorValor):
+    r = (a-b*q)
+    if rs >= r and r>0:
+        qs = q
+        rs = r
+print(qs,rs)
